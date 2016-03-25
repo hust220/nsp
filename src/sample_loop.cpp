2412f4ad8293b7565f8ac45369e55332f1dafb99
jian <wj_hust08@163.com>
2016-03-25 10:58:05 +0800

    2016-3-25

---
+++ src/sample_loop.cpp
@@ -0,0 +1,24 @@
+#include "nsp.hpp"
+#include <jian/nuc3d/SplitModel.hpp>
+#include <jian/nuc3d/SampleLoop.hpp>
+
+namespace jian {
+
+REGISTER_NSP_COMPONENT(sample_loop) {
+    loop *l;
+    std::shared_ptr<Model> model;
+    SplitModel split_model(par["model"][0], par["seq"][0], par["ss"][0]);
+    LOOP_TRAVERSE(split_model._ss_tree.head,
+        if (L->num_sons() == 3) {
+            l = L;
+            model = std::make_shared<Model>(split_model._loops[l]);
+        }
+    );
+    SampleLoop sample_loop(l, *model);
+    for (int i = 0; i < JN_INT(par["num"][0]); i++) {
+        write_pdb(sample_loop(), par["out"][0] + JN_STR(i+1) + ".pdb");
+    }
+}
+
+} // namespace jian
+
