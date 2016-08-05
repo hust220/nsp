858216caf9acd328f36d3dc76e43122050683928
jian <wj_hust08@163.com>
2016-08-05 16:05:54 +0800

    2016-8-5

---
+++ src/ss_pred.cpp
@@ -0,0 +1,14 @@
+#include "nsp.hpp"
+#include <jian/nuc2d/ss_pred.hpp>
+
+namespace jian {
+
+REGISTER_NSP_COMPONENT(ss_pred) {
+    std::string seq = par["seq"][0];
+    std::string ss = ss_pred(seq);
+    std::cout << seq << std::endl;
+    std::cout << ss << std::endl;
+}
+
+} // namespace jian
+
