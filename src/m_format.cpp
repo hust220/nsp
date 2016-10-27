8f6741aa36780a22824639567e7fda14285d9ae4
Jian <wj_hust08@163.com>
2016-10-27 21:04:26 +0800

    2016-10-27

--- src/m_format.cpp
+++ src/m_format.cpp
@@ -1,27 +1,30 @@
 #include "nsp.hpp"
 #include <jian/pdb.hpp>
+#include <jian/nuc3d/Format.hpp>
 
 namespace jian {
+	namespace {
+		void m_format(const Par &par, std::string mol_type) {
+			std::string in = par.get("s", "pdb", "i", "in");
+			std::string out = par.get("o", "out");
+			std::ofstream ofile;
+			Format format;
+			Molecule mol;
 
-	REGISTER_NSP_COMPONENT(rna) {
-		std::string in = par.get("s", "pdb", "i", "in");
-		std::string out = par.get("o", "out");
-		std::ofstream ofile;
+			FOPEN(ofile, out);
+			mol_read(mol, in, mol_type);
+			format.sort(mol);
+			ofile << mol << std::endl;
+			FCLOSE(ofile);
+		}
 
-		FOPEN(ofile, out);
-		ofile << mol_read_to<Molecule>(in, "RNA") << std::endl;
-		FCLOSE(ofile);
+		REGISTER_NSP_COMPONENT(rna) {
+			m_format(par, "RNA");
+		}
+
+		REGISTER_NSP_COMPONENT(dna) {
+			m_format(par, "DNA");
+		}
 	}
-
-	REGISTER_NSP_COMPONENT(dna) {
-		std::string in = par.get("s", "pdb", "i", "in");
-		std::string out = par.get("o", "out");
-		std::ofstream ofile;
-
-		FOPEN(ofile, out);
-		ofile << mol_read_to<Molecule>(in, "DNA") << std::endl;
-		FCLOSE(ofile);
-	}
-
 } // namespace jian
 
