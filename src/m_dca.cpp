e3e03c2e160a740ced4ffbfe5d85993a1fa95c33
Jian <wj_hust08@163.com>
2016-11-11 20:28:56 +0800

    aa

--- src/m_dca.cpp
+++ src/m_dca.cpp
@@ -8,11 +8,12 @@ REGISTER_NSP_COMPONENT(dca) {
     int n = 1;
     float step = 1;
 	std::string mol_type = "RNA";
+	std::string method = "mf";
 
     std::string out_file = par.get("o", "out");
     std::string fa_file = par.get("i", "in");
-    std::string method = par.get("m", "method");
 
+    par.set(method, "m", "method");
     par.set(n, "n");
     par.set(step, "step");
 	par.set(mol_type, "t", "type");
