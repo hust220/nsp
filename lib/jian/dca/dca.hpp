015f3af8f3ab6a67a1080c33964575aaa97343fb
Jian <wj_hust08@163.com>
2016-11-28 20:35:21 +0800

    aa

---
+++ lib/jian/dca/dca.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+namespace jian {
+namespace dca {
+
+void analyze(std::string fa_file, std::string out_file, int n);
+
+} // namespace dca
+} // namespace jian
+
+
