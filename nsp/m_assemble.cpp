f16c330bd9f436bb6e44dc85934689b319c2781b
Jian <wj_hust08@163.com>
2017-01-18 09:48:07 +0800

    aa

--- nsp/m_assemble.cpp
+++ nsp/m_assemble.cpp
@@ -17,12 +17,12 @@ REGISTER_NSP_COMPONENT(assemble) {
     ass.assemble();
 
 	n = 1;
-	mol_write(ass._pred_chain, to_str(ass._name, ".assemble.", n, ".pdb"));
+	mol_write(ass._pred_chain, to_str(ass._name, ".pred.", n, ".pdb"));
 	for (n = 2; n <= num; n++) {
 		ass.sample_all_templates();
 		ass.assemble();
 		ass.log << "# Writing sampling structure " << n << std::endl;
-		mol_write(ass._pred_chain, to_str(ass._name, ".assemble.", n, ".pdb"));
+		mol_write(ass._pred_chain, to_str(ass._name, ".pred.", n, ".pdb"));
 	}
 
 }
@@ -32,37 +32,37 @@ REGISTER_NSP_COMPONENT(sample) {
 
 	std::ostringstream stream;
 	int n;
-//	bool write_samplings = par.has("write_samplings");
-	bool write_samplings = true;
+	bool write_samplings = par.has("write_samplings");
+	//bool write_samplings = true;
 
 	int num = 1;
 	par.set(num, "n", "num");
 
-//	int num_samplings = 100 * num;
-//	par.set(num_samplings, "num_samplings");
+	int num_samplings = 100 * num;
+	par.set(num_samplings, "num_samplings");
 
 	ass.select_templates();
     ass.assemble();
 
 	n = 1;
 	std::deque<Chain> chains;
-//	chains.push_back(std::move(ass._pred_chain));
+	chains.push_back(std::move(ass._pred_chain));
 
 	if (write_samplings) mol_write(ass._pred_chain, to_str(ass._name, ".sample.", n, ".pdb"));
-	for (n = 2; n <= num; n++) {
+	for (n = 2; n <= num_samplings; n++) {
 		ass.sample_one_template();
 		ass.assemble();
-//		chains.push_back(std::move(ass._pred_chain));
+		chains.push_back(std::move(ass._pred_chain));
 		ass.log << "# Writing sampling structure " << n << std::endl;
 		if (write_samplings) mol_write(ass._pred_chain, to_str(ass._name, ".sample.", n, ".pdb"));
 	}
 
-//	ass.log << "# Clustering..." << std::endl;
-//	auto result = pdb::cluster_chains(chains, n);
-//
-//	for (int i = 0; i < n; i++) {
-//		mol_write(chains[result[i][0]], to_str(ass._name, ".pred.", i + 1, ".pdb"));
-//	}
+	ass.log << "# Clustering..." << std::endl;
+	auto result = pdb::cluster_chains(chains, num);
+
+	for (int i = 0; i < num; i++) {
+		mol_write(chains[result[i][0]], to_str(ass._name, ".pred.", i + 1, ".pdb"));
+	}
 
 }
 
