76ff60a04c2a04112797b6c2aef3bd2af601f2e6
wangjian <wj_hust08@hust.edu.cn>
2017-02-02 18:01:10 +0800

    aa

--- test/main.cpp
+++ test/main.cpp
@@ -1,9 +1,9 @@
-#include <jntest/UnitTest.hpp>
+#include <jian/test.hpp>
 #include <jian/mpi.hpp>
 
 int main(int argc, char **argv) {
     JN_MPI_INIT(argc, argv);
-    run_test()
+    jian::run_test()
     JN_MPI_FREE;
 }
 
